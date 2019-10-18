7513c98f558ee12e6acf01093491f3cdfbd3f9bf
Phil Asmar <phil.asmar@gmail.com>
2019-10-18 16:27:33 -0400

    Merged 2 projects

---
+++ Audio_visualizer/Assets/Scripts/LyricLine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class LyricLine
+{
+    public double time { get; set; }
+    public string text { get; set; }
+
+    public LyricLine(double time, string text)
+    {
+        this.time = time;
+        this.text = text;
+    }
+}
---
+++ Audio_visualizer/Assets/SongSelection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SongSelection : MonoBehaviour
+{
+
+    public void songSelect()
+	{
+        Properties.selectedSong = "Killshot";
+		SceneManager.LoadScene("Music Room", LoadSceneMode.Single);
+	}
+}
