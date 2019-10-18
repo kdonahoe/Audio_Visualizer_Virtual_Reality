0cae2ef78936aa3ce3ac0d83b66e07db82753f2e
Katie <kd459@cornell.edu>
2019-10-18 15:13:30 -0400

    Audio Visualizer Part 1
    Audio Visualizer: parses spectrum of audio song using FFT. Then displays the spectrum as 256 moving cubes placed in circle fomration that surround a user

---
+++ Audio_visualizer/Assets/Scripts/SparkBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SparkBehavior : MonoBehaviour
+{
+    public Vector3 startVelocity;
+    public float delay;
+    public float shrinkSmooth;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Random.rotation * startVelocity;
+        Invoke("Die", delay);
+
+        var particleRenderer = transform.GetComponent<Renderer>();
+        particleRenderer.material.SetColor("_Color", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+    }
+
+    private void Update()
+    {
+        //scale localtozero
+        Vector3.Lerp(transform.localScale, Vector3.zero, shrinkSmooth);
+
+        if(transform.position.y <= 0)
+        {
+        //    Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
