4f0f642086fac3eb1516dea57a6c2da499047e28
Phil Asmar <phil.asmar@gmail.com>
2019-11-01 12:28:06 -0400

    Renamed Folder

---
+++ InDaBeat/Assets/JSON/LyricResponse.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class LyricResponse
+{
+    public Meta meta { get; set; }
+    public Response response { get; set; }
+
+    public class Meta
+    {
+        public int status { get; set; }
+    }
+
+    public class Stats
+    {
+        public int unreviewed_annotations { get; set; }
+        public int concurrents { get; set; }
+        public bool hot { get; set; }
+        public int pageviews { get; set; }
+    }
+
+    public class PrimaryArtist
+    {
+        public string api_path { get; set; }
+        public string header_image_url { get; set; }
+        public int id { get; set; }
+        public string image_url { get; set; }
+        public bool is_meme_verified { get; set; }
+        public bool is_verified { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public int iq { get; set; }
+    }
+
+    public class LyricObject
+    {
+        public int annotation_count { get; set; }
+        public string api_path { get; set; }
+        public string full_title { get; set; }
+        public string header_image_thumbnail_url { get; set; }
+        public string header_image_url { get; set; }
+        public int id { get; set; }
+        public int lyrics_owner_id { get; set; }
+        public string lyrics_state { get; set; }
+        public string path { get; set; }
+        public string pyongs_count { get; set; }
+        public string song_art_image_thumbnail_url { get; set; }
+        public string song_art_image_url { get; set; }
+        public Stats stats { get; set; }
+        public string title { get; set; }
+        public string title_with_featured { get; set; }
+        public string url { get; set; }
+        public PrimaryArtist primary_artist { get; set; }
+    }
+
+    public class Hit
+    {
+        public List<object> highlights { get; set; }
+        public string index { get; set; }
+        public string type { get; set; }
+        public LyricObject result { get; set; }
+    }
+
+    public class Response
+    {
+        public List<Hit> hits { get; set; }
+    }
+}
---
+++ InDaBeat/Assets/Utilities/WebUtils.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using UnityEngine;
+using static LyricResponse;
+
+public class WebUtils
+{
+    public LyricResponse searchSongs(string song)
+    {
+        try
+        {
+            string response = SendRequestToSpotify("GET", Properties.genius_search_url + song, "");
+            return JsonConvert.DeserializeObject<LyricResponse>(response);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public List<string> getTopLyrics(string song)
+    {
+        List<string> lyrics = new List<string>(); 
+        try
+        {
+            string txtResponse = SendRequestToSpotify("GET", Properties.genius_search_url + song, "");
+            LyricResponse response = JsonConvert.DeserializeObject<LyricResponse>(txtResponse);
+            if (response != null)
+            {
+                if (response.response.hits.Count > 0)
+                {
+                    HtmlWeb web = new HtmlWeb();
+                    HtmlDocument doc = web.Load(response.response.hits[0].result.url);
+                    foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//div[@class='lyrics']//p").Elements())
+                    {
+                        if (!string.IsNullOrWhiteSpace(node.InnerHtml))
+                        {
+                            lyrics.Add(node.InnerHtml);
+                        }
+                    }
+                    return lyrics;
+                }
+            }
+            return null;
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+            return null;
+        }
+    }
+
+    public string SendRequestToSpotify(String method, String url, String jsonBody)
+    {
+        HttpWebRequest request;
+        request = (HttpWebRequest)WebRequest.Create(url);
+        // Add the method to authentication the user
+        request.Method = method;
+        request.Accept = "application/json";
+        request.Headers.Add("Authorization", "Bearer " + Properties.access_token);
+        request.ContentLength = jsonBody.Length;
+        request.Timeout = 14400000;
+        request.KeepAlive = false;
+        string body = jsonBody;
+
+        byte[] bodyBytes = Encoding.UTF8.GetBytes(body);
+
+        if (!string.IsNullOrEmpty(body))
+        {
+            request.ContentType = "application/json";
+            request.ContentLength = bodyBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bodyBytes, 0, bodyBytes.Length);
+            requestStream.Close();
+        }
+        try
+        {
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+}
