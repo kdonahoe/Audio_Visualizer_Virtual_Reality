b1279b63985667569c97b5d03544148a583888bc
Phil Asmar <phil.asmar@gmail.com>
2019-11-05 11:04:42 -0500

    Added new music

---
+++ InDaBeat/Assets/Utilities/Properties 2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Properties
+{
+    public static string access_token = "_kv_SHyyjbxJVv3DxgmPdJX9P_nv5q5QbT_B0IMO8O0CHRQqqucGYebw9hPDXOw6";
+    public static string genius_search_url = "https://api.genius.com/search?q=";
+    public static string selectedSong = "Queen - Fat Bottomed Girls";
+    public static string[] availableSongs = { "Chris Brown - No Guidance", "Eminem - Killshot", "Marvin Gaye - Ain't No Mountain High Enough", "Queen - Fat Bottomed Girls" };
+}
